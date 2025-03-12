69af4b83ab7fb9566b5f3635aa911eb53f49c4d4
Catpawz <cat@catpawz.net>
2025-03-12 12:34:00 +0100

    Update GitHub link in footer and copyright year to 2024

--- inc/footer.php
+++ inc/footer.php
@@ -3,7 +3,7 @@
             <div class="max-w-3xl mx-auto">
                     <ul class="text-lg flex items-center justify-center flex-col gap-7 md:flex-row md:gap-12 transition-all duration-500 py-16 mb-10">
                         <li><a href="https://catpawz.net" class="text-green-200 hover:text-green-300">Main website</a></li>
-                        <li><a href="https://github.com/french-femboi/legal-website" class="text-green-200 hover:text-green-300">GitHub</a></li>
+                        <li><a href="https://github.com/catpawzz/legal-website" class="text-green-200 hover:text-green-300">GitHub</a></li>
                     </ul>
                     <span class="text-lg text-gray-100 text-center block">©<a href="https://catpawz.net">Catpawzz</a> 2024 - Present, All rights reserved.</span>
             </div>
